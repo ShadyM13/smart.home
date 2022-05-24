f5d257188d66c804a5515734bef79cdb18459557
Shady Abdulmajeed <ShadyAbdul96@gmail.com>
2022-05-24 18:45:59 +0200

    Added a variable x

--- main.c
+++ main.c
@@ -5,4 +5,29 @@
  *      Author: lover
  */
 
+#define F_CPU 8000000L
+#include <util/delay.h>
+#include <stdlib.h>
 
+#include "LBIT_math.h"
+#include "LSTD_types.h"
+
+
+#include "MDIO_interface.h"
+#include "MADC_interface.h"
+#include "MGIE_interface.h"
+#include "MSPI_interface.h"
+#include "MTIMER_interface.h"
+#include "MEXTI_interface.h"
+#include "MGIE_interface.h"
+
+
+#include "HLCD_interface.h"
+#include "HKPD_interface.h"
+
+int main(void)
+{
+	int x;
+
+	return 0;
+}
